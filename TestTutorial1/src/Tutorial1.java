15e993cc61d8853086029762658ade1ecdc1e035
EgemenYapucu <56233612+EgemenYapucu@users.noreply.github.com>
2022-10-15 14:53:08 +0300

    Update Tutorial1.java

--- TestTutorial1/src/Tutorial1.java
+++ TestTutorial1/src/Tutorial1.java
@@ -3,9 +3,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tutorial1 {
 	
 	public static void main(String[] args) {
+		
+		// declaration and instantiation of objects/variables
 		System.setProperty("webdriver.chrome.driver","D:\\Browser drivers\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		
+		// launch Chrome and direct it to the google.com
 		driver.get("https://www.google.com");
+		
+		//close Chrome
+		driver.close();
 	}
 
 };
+
